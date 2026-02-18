e0a1fdf605c7c15a8e86e04955f90e3f0d523405
Danieldaviddf <76775661+Danieldaviddf@users.noreply.github.com>
2026-02-18 10:33:28 +0000

    final

---
+++ app.js
@@ -0,0 +1,16 @@
+
+ let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let final = ['.es', '.com', '.net'];
+
+  for (let index = 0; index < pronoun.length; index++) {
+    for (let i = 0; i < adj.length; i++) {
+        for (let e = 0; e < noun.length; e++) {
+            for (let x = 0; x < final.length; x++) {
+              console.log(pronoun[index]+adj[i]+noun[e]+final[x]);  
+                
+            }
+        }
+    }
+  }
