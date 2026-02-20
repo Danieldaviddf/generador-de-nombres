23da30c50026e1bad82fe085cfa850032694bf40
Danieldaviddf <76775661+Danieldaviddf@users.noreply.github.com>
2026-02-20 09:33:08 +0000

    actualizacion

--- src/app.js
+++ src/app.js
@@ -5,7 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+ let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let final = ['.es', '.com', '.net'];
+
+  for (let index = 0; index < pronoun.length; index++) {
+    for (let i = 0; i < adj.length; i++) {
+        for (let e = 0; e < noun.length; e++) {
+            for (let x = 0; x < final.length; x++) {
+              console.log(pronoun[index]+adj[i]+noun[e]+final[x]);  
+                
+            }
+        }
+    }
+  }
